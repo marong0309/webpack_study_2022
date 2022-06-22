fa0de511aeb9ea20df3deea6880ae367463da3dd
이준규 <ljk6934@jongdali.com>
2022-06-22 16:39:27 +0900

    webpack study

---
+++ es6-modules/js/app.js
@@ -0,0 +1,4 @@
+// 상대경로가 지정되면 emmit에 함수명이 뜸
+import { sum } from './math'
+console.log('10 + 20 =', sum(10, 20));
+
---
+++ es6-modules/js/math.js
@@ -0,0 +1,4 @@
+// sum() 함수 구현 후 export
+export function sum(a, b){
+    return a + b;
+}
